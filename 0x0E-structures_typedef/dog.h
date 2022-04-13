1c1b10f3d797a7233c422f22c9b3216dffa57ff5
josephmenza <josephmenza299@gmail.com>
2022-04-13 21:02:16 +0300

    structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+struct dog
+{
+ char *name;
+ float *age;
+ char *owner;
+}
+int main(void)
+{
+struct dog my_dog;
+my_dog.name="jacky";
+my_dog.age=9;	
+myy_dog.owner="belete";
+return(0);
+}
