efda4a64cccf7a00d2a51a11d5eaedc392623fb7
josephmenza <josephmenza299@gmail.com>
2022-04-13 22:05:48 +0300

    structures_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints dog data
+ * @d: dog struct
+ */
+
+
+int main(void)
+{
+struct dog*d
+ dog=*d("jacky", 9, "belete")
+ if(user==null)
+ {
+printf("name %s created !/n",*d->name);
+printf("age %d year old !/n",*d->age);
+printf("owner %s the owner !/n",*d->owner)
+return(0);
+ }
+}
