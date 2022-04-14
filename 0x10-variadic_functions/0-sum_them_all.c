f7958e012477da22fa0111606e133fd0b5f12e0d
josephmenza <josephmenza299@gmail.com>
2022-04-14 14:15:00 +0300

    variadic_function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,18 +7,17 @@
  * Return: sum of args
  * On error, 0 is returned
  */
-int sum_them_all(const int n, ...)
+int sum_them_all(const unsigned int n, ...)
 {
 va_list list;
-int i, sum=0;
+int unsigned i, sum = 0;
 
-
-va_start(list,n);
+va_start(list, n);
 if (n != 0)
 	for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
 	;
 va_end(list);
 
 return (sum);
-
 }
+
